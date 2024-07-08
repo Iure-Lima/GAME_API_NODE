68eea2d01f86dd0f14f5fc655a16ddb324318d54
IureLima <iurelimasilva@gmail.com>
2024-07-08 07:53:39 -0300

    feat[router]: Route handling

--- src/routers/indexRouters.js
+++ src/routers/indexRouters.js
@@ -4,5 +4,8 @@ import gameRouter from "./game.js";
 const router = Router();
 
 router.use("/game", gameRouter);
+router.all("*", (req, res) => {
+  res.status(404).json({ message: "Page not found" });
+});
 
 export default router;

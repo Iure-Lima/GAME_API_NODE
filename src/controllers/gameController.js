6666caa69840e47145112e3b4c1ca62a6d626bc6
IureLima <iurelimasilva@gmail.com>
2024-07-08 10:40:47 -0300

    feat[game controller]: create the controller methods

---
+++ src/controllers/gameController.js
@@ -0,0 +1,20 @@
+const createdGame = async (req, res) => {};
+
+const getAllGame = async (req, res) => {};
+
+const getGameByID = async (req, res) => {};
+
+const updateGame = async (req, res) => {};
+
+const partialUpdateGame = async (req, res) => {};
+
+const deleteGame = async (req, res) => {};
+
+export default {
+  createdGame,
+  getAllGame,
+  getGameByID,
+  updateGame,
+  partialUpdateGame,
+  deleteGame,
+};
